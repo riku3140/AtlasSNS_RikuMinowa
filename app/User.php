7a11304affcea5b1b1c7d3789618a893c32d04d1
箕輪陸 <>
2024-05-16 12:45:23 +0900

    no message

--- app/User.php
+++ app/User.php
@@ -25,6 +25,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+ //ユーザーがフォローしている、人数の取得
+    public function follows(){
+        return $this -> belongsToMany('App\User','follows','following_id','followed_id');
+    }
 
+    public function follow(){
+        return $this -> belongsToMany('App\User','follows','following_id','followed_id');
+    }
+
+    //ユーザーをフォローしている、フォロワー人数の取得
+    public function follower(){
+        return $this ->belongsToMany('App\User','follows', 'following_id','following_id');
+    }
+
+    //フォロー人数の取得
+    public function isFollowing(Int $user_id){
+        return (boolean) $this ->follows() -> where('followed_id',$user_id)->first();
+
+    }
+
+    //フォロワー人数の取得
+    public function isFollowed(Int $user_id){
+        return (boolean) $this -> followers() -> where('following_id',$user_id) ->first(['follows.id']);
+    }
 
 }
