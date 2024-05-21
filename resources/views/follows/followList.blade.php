38593eba6de7148c885e14c73cf2ec2d7a0e3427
箕輪陸 <>
2024-05-22 06:41:38 +0900

    no message

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -5,7 +5,7 @@
     <h1>フォローリスト</h1>
     <div class="follow_icon">
         @foreach($follows as $follow)
-        <a><img src="{{ asset('storage/'.$follow->images) }}" alt="フォローアイコン"></a>
+        <a><img src="{{ asset('public/storage'.$follow->images) }}" alt="フォローアイコン"></a>
         @foreach($posts as $post)
     <p>名前:{{ $post->user->username }}</p>
     <p>投稿内容:{{ $post->post }}</p>
